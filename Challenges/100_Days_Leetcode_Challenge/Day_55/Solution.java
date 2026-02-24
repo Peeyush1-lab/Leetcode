9adeb6decff5b735d1f7851338a6ef90ccbd6185
Peeyush Tiwari <115945243+Peeyush1-lab@users.noreply.github.com>
2026-02-24 22:18:02 +0530

    Updated day 55 problem code

--- Challenges/100_Days_Leetcode_Challenge/Day_55/Solution.java
+++ Challenges/100_Days_Leetcode_Challenge/Day_55/Solution.java
@@ -1,12 +1,3 @@
-// Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 public class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         int zeros = 0;
