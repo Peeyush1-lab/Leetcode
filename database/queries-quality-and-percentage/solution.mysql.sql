0b40ec5ef75ad35ce1182a955061e1403f857ed5
Peeyush Tiwari <tiwari.peeyush2006@gmail.com>
2026-09-08 23:55:56 +0530

    Solve Queries Quality and Percentage [mysql]

---
+++ database/queries-quality-and-percentage/solution.mysql.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+Select query_name, round(sum(rating/position)/count(query_name),2) as quality, round((count(case when rating < 3  then 1 end)/count(query_name))*100,2) as poor_query_percentage from Queries group by query_name;
