5a3f7800eba22775e0d81a7f06514c4d27c04f1e
Peeyush Tiwari <tiwari.peeyush2006@gmail.com>
2026-09-19 15:20:05 +0530

    Solve Contains Duplicate [java]

---
+++ contains-duplicate/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> arr = new HashSet<>();
+        for(int i : nums)
+        {
+            if(!arr.add(i))
+            {
+                return true;
+            }
+            arr.add(i);
+        }
+        return false;
+    }
+}
