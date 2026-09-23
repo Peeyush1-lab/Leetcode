fecf4f86856b1d81e3ac2d84a1997066402b45b4
Peeyush Tiwari <tiwari.peeyush2006@gmail.com>
2026-09-24 00:46:38 +0530

    Solve Valid Palindrome [java]

--- valid-palindrome/solution.java
+++ valid-palindrome/solution.java
@@ -6,12 +6,14 @@ class Solution {
 
         while (left < right) {
 
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+            if (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
+                continue;
             }
 
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            if (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
+                continue;
             }
 
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
